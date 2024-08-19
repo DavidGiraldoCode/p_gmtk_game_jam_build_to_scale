ddcff012433580d0a38748f5fbe276cb417b8ebe
DavidGiraldoCode <davidgiraldodesign@gmail.com>
2024-08-19 17:07:09 +0200

    Build successful

--- Assets/Scripts/Controllers/ExitParticlesController.cs
+++ Assets/Scripts/Controllers/ExitParticlesController.cs
@@ -1,6 +1,4 @@
 using UnityEngine;
-using UnityEngine.Rendering.VirtualTexturing;
-
 public class ExitParticlesController : MonoBehaviour
 {
     [SerializeField] private GameObject m_exitSparksParticlesPrefab;
